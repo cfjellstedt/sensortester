93c3db73514e6d0048ce7cc3b6cff0db536bcde5
cfjellstedt <43853419+cfjellstedt@users.noreply.github.com>
2018-11-02 10:18:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const https = require('https');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+const host = 'api.thingspeak.com';
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/webhook", function(req, res) {
+
+ //let unit = req.body.queryResult.parameters['unit']; // take out the unit, lamp e.g.
+ //let state = req.body.queryResult.parameters['state']; // take out the the state, on or off
+ 
+  callThingApi().then((output) => {
+    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': 'something is wrong' });
+  });
+ 
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+
+
+
+function callThingApi () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+    // Make the HTTP request
+	
+    https.get('https://api.thingspeak.com/channels/617048/feeds.json?results=1', (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let temp = response.feeds['field1'];
+        // Create response
+        let output = 'The temperature is ' + temp;
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
+
+
